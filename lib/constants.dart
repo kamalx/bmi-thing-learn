a567ec9822855bd117803b39b245452a23189d91
Kamal Morjal <kamal@mulberries.xyz>
2020-10-26 20:34:55 +0530

    update some constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -6,7 +6,7 @@ const k_LABEL_TEXT_STYLE = TextStyle(
 );
 
 const k_LABEL_NUMBER_TEXT_STYLE = TextStyle(
-  fontSize: 67.0,
+  fontSize: 50.0,
   fontWeight: FontWeight.w900,
 );
 
@@ -14,5 +14,5 @@ const k_ACTIVE_CARD = Color(0xFF1D1E33);
 const k_INACTIVE_CARD = Color(0xFF111328);
 const k_CTA_COLOR = Color(0xFFEB1555);
 const k_CTA_HEIGHT = 80.0;
-const k_MIN_HEIGHT = 120.0;
-const k_MAX_HEIGHT = 220.0;
+const k_MIN_HEIGHT_VALUE = 120.0;
+const k_MAX_HEIGHT_VALUE = 220.0;
