71d31bafeb94eb478e1fc6ecd8c99925d1001bde
Kamal Morjal <kamal@mulberries.xyz>
2020-10-26 13:56:39 +0530

    add some more constants
    some more constants from the codebase go into the common file

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,3 +4,8 @@ const LABEL_TEXT_STYLE = TextStyle(
   fontSize: 18,
   color: Color(0xFFD8DE98),
 );
+
+const ACTIVE_CARD = Color(0xFF1D1E33);
+const INACTIVE_CARD = Color(0xFF111328);
+const CTA_COLOR = Color(0xFFEB1555);
+const CTA_HEIGHT = 80.0;
